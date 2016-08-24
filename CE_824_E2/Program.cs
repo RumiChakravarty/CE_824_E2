4677271432df8a53570cbe8821e0df3673ebc8e2
RumiChakravarty <rumi.sarma.c@gmail.com>
2016-08-24 12:51:02 -0400

    new example of method

--- CE_824_E2/Program.cs
+++ CE_824_E2/Program.cs
@@ -10,6 +10,44 @@ namespace CE_824_E2
     {
         static void Main(string[] args)
         {
+            Boolean ex_bool = true;
+            Console.WriteLine(ex_bool);
+            myTest("dotNet");
+            myWrite("Good Morning");
+            Console.Write("Roses ");
+            Console.Write("are\n");
+            Console.WriteLine("   R E D\n");
+            Console.Write("Violets are \n");
+            Console.Write("Blue.\n");
+            Console.WriteLine("I am a developer");
+            Console.Write("  and so are you\n");
+
+            // string qq = writeSomething();
+            //Console.Write(qq);
+            Console.Write(writeSomething());
         }
+        static void myWrite(string message)
+        {
+            Console.Write(message + "pizzaz");
+            Console.Write("ubl" + message + "pizza");
+         
+
+        }
+        static void myTest(string msg)
+        {
+            Console.WriteLine("This class is Fun as we r learning new things");
+            Console.Write(msg + "   Class");
+            Console.ReadLine();
+
+        }
+        static string writeSomething()
+        {
+            return @"Please write something
+
+We will have to do more during Lab hour
+
+Thank You";
+        }
+
     }
 }
